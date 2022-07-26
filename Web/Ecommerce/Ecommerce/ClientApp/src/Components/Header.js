2c7fd809b09d6f080544f3fe68dcd5d9185c7ab8
yass_ <yas.matosc@gmail.com>
2022-07-26 16:59:03 -0300

    Ecommerce - 26.07 - Criação do Header

--- Web/Ecommerce/Ecommerce/ClientApp/src/Components/Header.js
+++ Web/Ecommerce/Ecommerce/ClientApp/src/Components/Header.js
@@ -1,9 +1,22 @@
 ﻿import React from 'react'
 import '../Css/Header.css'
+import { AiOutlineMenu } from 'react-icons/ai'
 
 const Header = () => {
-    return <header className='header row'>
-       
+   return <header className='header row'>
+        <div className='logo col-sm-3 col-5'>
+           <p>AIR BRAKES</p>
+        </div>
+
+       <div className='links col-sm-9 col-7'>
+           <div className='router'>
+               
+            </div>
+
+           <div className='responsive'>
+               <AiOutlineMenu size={30} style={{ cursor: 'pointer', userSelect: 'none' }} />
+            </div>
+        </div>
     </header>
 }
 
