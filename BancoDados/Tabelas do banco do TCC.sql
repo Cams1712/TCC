8f49d6019eb48abeb275ab6559152a90e64a2711
Thiago Aguiar <90070362+ThiagoAguiiar@users.noreply.github.com>
2022-08-31 15:10:37 -0300

    Update Tabelas do banco do TCC.sql

--- BancoDados/Tabelas do banco do TCC.sql
+++ BancoDados/Tabelas do banco do TCC.sql
@@ -14,6 +14,7 @@ CREATE TABLE caminhoes(
 	placa VARCHAR(8) NOT NULL PRIMARY KEY,
 	marca VARCHAR(20) NOT NULL,
 	modelo VARCHAR(20) NOT NULL,
+	bluetooth VARCHAR(20) NOT NULL,
 	piloto VARCHAR(10) NOT NULL,
 	ativo BOOL NOT NULL,
 	FOREIGN KEY (piloto) REFERENCES funcionarios (num_funcionario)
