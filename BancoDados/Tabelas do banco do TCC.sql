c22738243377e3c8f73ca5ea02e2e3f3eeb5e493
Thiago Aguiar <90070362+ThiagoAguiiar@users.noreply.github.com>
2022-10-01 18:33:30 -0300

    Update Tabelas do banco do TCC.sql

--- BancoDados/Tabelas do banco do TCC.sql
+++ BancoDados/Tabelas do banco do TCC.sql
@@ -2,7 +2,7 @@ use camila_gomes;
 
 CREATE TABLE funcionarios (
 	num_funcionario VARCHAR(10) NOT NULL PRIMARY KEY,
-	foto_perfil TEXT NOT NULL,
+	foto_perfil LONGTEXT NOT NULL,
 	nome VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL,
     	senha VARCHAR(100) NOT NULL,
