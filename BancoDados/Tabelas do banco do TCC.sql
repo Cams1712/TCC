57c338514846f6ee151f424ab4826c996012f494
Cams1712 <96265394+Cams1712@users.noreply.github.com>
2022-10-25 14:09:43 -0300

    Update Tabelas do banco do TCC.sql

--- BancoDados/Tabelas do banco do TCC.sql
+++ BancoDados/Tabelas do banco do TCC.sql
@@ -5,7 +5,7 @@ CREATE TABLE funcionarios (
 	foto_perfil LONGTEXT NOT NULL,
 	nome VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL,
-    	senha VARCHAR(100) NOT NULL,
+    	senha VARCHAR(20) NOT NULL,
 	adm BOOL NOT NULL,
 	ativo BOOL NOT NULL
 );
